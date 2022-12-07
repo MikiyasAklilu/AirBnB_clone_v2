acc99db9102a998b9387b958cb594c29ee984d5e
MikiyasAklilu <mikiyasaklilu.me@gmail.com>
2022-12-07 22:23:29 +0300

    AirBnB clone - MySQL

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["44.192.52.218", "54.90.39.103"]
 
 
 def do_deploy(archive_path):
